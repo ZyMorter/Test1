eb594948ac508721732cadbcb02e11eb85c3b0be
ZyMorter <75341737+ZyMorter@users.noreply.github.com>
2023-04-06 23:21:52 -0600

    Add files via upload

--- main.go
+++ main.go
@@ -2,41 +2,26 @@ package main
 
 import (
 	"log"
-	"math/rand"
 	"net/http"
-	
 )
 
+// The handler function:
+func bio(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("\nMy name is Zyon Jamaal Morter, I'm 20 years old. I was born on 31/12/02 and am pursuing an associate degree in Information Technology at UB."))
+}	
 
-// store random quotes:
-var quotes = []string{
-	"The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
-"The way to get started is to quit talking and begin doing. -Walt Disney",
-"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma  which is living with the results of other people's thinking. -Steve Jobs",
-"If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
-"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
-"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
-"Life is what happens when you're busy making other plans. -John Lennon",
-"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe. ― Albert Einstein",
-"In three words I can sum up everything I've learned about life: it goes on.― Robert Frost",
-
-}
-
-func random(w http.ResponseWriter, r *http.Request) {
-	// Get a random quote
-	quote := quotes[rand.Intn(len(quotes))]
-	//write the response
-	w.Write([]byte(quote))
-}
 func main() {
 	// Create a new serveMux
 	mux := http.NewServeMux()
-	mux.HandleFunc("/random", random)
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	mux.HandleFunc("/", bio)
+	log.Print("Starting server on :2000")
+	err := http.ListenAndServe(":2000", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
 } 
 
-//http://localhost:4000/random
+
+
+
+
